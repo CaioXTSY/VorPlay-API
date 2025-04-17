be7ce76db2de4530c5fbd1d0a77c1523133019ce
Caio <125697270+CaioXTSY@users.noreply.github.com>
2025-04-17 01:08:49 -0300

    feat: adicionar suporte para documentação da API com redoc-express

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
-import 'dotenv/config';                    
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+import redocExpressMiddleware from 'redoc-express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-
+  app.enableCors();
   const swaggerConfig = new DocumentBuilder()
     .setTitle('VorPlay API')
     .setDescription('Documentação da API do VorPlay')
@@ -21,9 +21,18 @@ async function bootstrap() {
     ignoreGlobalPrefix: false,
   });
 
+  app.use('/api/v1/swagger.json', (_req, res) => res.json(document));
   SwaggerModule.setup('api/v1/docs', app, document);
-
-  app.enableCors();
+  app.use(
+    '/api/v1/redoc',
+    redocExpressMiddleware({
+      title: 'VorPlay API Docs',
+      specUrl: '/api/v1/swagger.json',
+      redocOptions: {
+        theme: { colors: { primary: { main: '#dd2c00' } } },
+      },
+    }),
+  );
 
   await app.listen(3000);
   console.log(`🚀 Server running on: ${await app.getUrl()}/api/v1`);
